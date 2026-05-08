b8275d04902b3b550cca5c4fdfc3ffccf404195c
Kieran Cooke <cookekieran3@gmail.com>
2026-05-08 14:00:08 +0100

    upload metadata to hugging face

---
+++ part_b/upload_metadata.py
@@ -0,0 +1,33 @@
+import os
+from huggingface_hub import HfApi
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# --- CONFIG ---
+HF_REPO_ID = "cookekieran/eu_cities"
+TRAIN_PATH = os.getenv("TRAIN_METADATA_PATH")
+TEST_PATH = os.getenv("TEST_METADATA_PATH")
+HF_TOKEN = os.getenv("HF_TOKEN")
+
+api = HfApi()
+
+# 1. Upload Training CSV
+api.upload_file(
+    path_or_fileobj=TRAIN_PATH,
+    path_in_repo="train_metadata.csv",
+    repo_id=HF_REPO_ID,
+    repo_type="dataset",
+    token=HF_TOKEN
+)
+
+# 2. Upload Testing CSV
+api.upload_file(
+    path_or_fileobj=TEST_PATH,
+    path_in_repo="test_metadata.csv",
+    repo_id=HF_REPO_ID,
+    repo_type="dataset",
+    token=HF_TOKEN
+)
+
+print("CSVs uploaded.")
